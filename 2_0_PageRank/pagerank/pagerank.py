e94925e807331264cf38cc42ad64614545eb48c2
JokusPokus <jakob.schmitt@code.berlin>
2020-04-20 18:38:22 +0200

    implement PageRank

--- 2_0_PageRank/pagerank/pagerank.py
+++ 2_0_PageRank/pagerank/pagerank.py
@@ -2,6 +2,7 @@ import os
 import random
 import re
 import sys
+from copy import deepcopy
 
 DAMPING = 0.85
 SAMPLES = 10000
@@ -57,7 +58,26 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    prob_dist = dict()
+
+    # If page doesn't link to any other site
+    if not corpus[page]:
+        prob = 1 / len(corpus)
+        for site in corpus:
+            prob_dist[site] = prob
+
+        return prob_dist
+
+    for site in corpus:
+        # Choose site in corpus at random
+        rand = (1 - damping_factor) / len(corpus)
+
+        # Choose link from page at random
+        linked = damping_factor * (1 / len(corpus[page])) if site in corpus[page] else 0
+
+        prob_dist[site] = rand + linked
+
+    return prob_dist
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,7 +89,22 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    counts = dict()
+    current_page = random.choice([*corpus])
+    counts[current_page] = 1
+    for i in range(n-1):
+        # Weights for choosing next link based on current page's transition function
+        transition = transition_model(corpus, current_page, damping_factor)
+        sites = [site for site in corpus]
+        weights = [transition[site] for site in sites]
+        current_page = random.choices(sites, weights)[0]
+        counts[current_page] = counts.get(current_page, 0) + 1
+
+    # Compute and return relative frequencies
+    pagerank_s = dict()
+    for site in corpus:
+        pagerank_s[site] = counts.get(site, 0) / n
+    return pagerank_s
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -81,7 +116,41 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    pagerank_i = dict()
+
+    # Initial probability for picking any site
+    prob = 1 / len(corpus)
+    for site in corpus:
+        pagerank_i[site] = prob
+
+    # Damping part of the formula is constant:
+    damp = (1 - damping_factor) / len(corpus)
+
+    converged = False
+
+    # Repeat estimation until convergence
+    while not converged:
+        before = deepcopy(pagerank_i)
+
+        # Consider pagerank component through pages that do not link to any other sites
+        non_linking_sites = sum([pagerank_i[page] / len(corpus) if not corpus[page] else 0 for page in corpus])
+
+        for site in pagerank_i:
+            new_value = non_linking_sites
+            # Add component for each page that links to the site
+            new_value += sum([before[page] / len(corpus[page]) if (site in corpus[page]) else 0 for page in corpus])
+
+            # Update pagerank according to formula
+            pagerank_i[site] = damp + damping_factor * new_value
+
+        # Assume convergence and try to negate it
+        converged = True
+        for site in pagerank_i:
+            if abs(pagerank_i[site] - before[site]) > 0.001:
+                converged = False
+                break
+
+    return pagerank_i
 
 
 if __name__ == "__main__":
